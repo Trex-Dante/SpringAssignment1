0d88479f3b22e835ecd4d48d745f054333b40df9
Katywa Aphelele <ndayiavumile63@gmail.com>
2025-04-08 01:17:38 +0200

    Update CourseSelector.java

--- src/main/java/com/group4/SpringAssignement/CourseSelector.java
+++ src/main/java/com/group4/SpringAssignement/CourseSelector.java
@@ -1,24 +1,30 @@
 package com.group4.SpringAssignement;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @Service
 public class CourseSelector {
 
+    // Initialize with three different arrays
+    List<Courses> courses = new ArrayList<>();
 
-    @Autowired
-    Courses courses;
-
-    String[] FoundationCourses = new String[] {"Introduction to computer science", "Data Structure"};
-
-    String[] UndergraduateCourses = new String[] {"Algorithms", "Computer network", "Software Engineering"};
-
-    String[] HonorsCourses = new String[] {"Advanced Algorithms", "Machine Learning", "Distributed systems", "Cybersecurity"};
-
-
+    public CourseSelector() {
+        // Initialize with 3 arrays containing 2, 4, and 5 items respectively
+        courses.add(new Courses("Foundation Courses", new String[]{"Introduction to computer science", "Data Structure"}));
+        courses.add(new Courses("Undergraduate Courses", new String[]{"Algorithms", "Computer network", "Software Engineering"}));
+        courses.add(new Courses("Honors Courses", new String[]{"Advanced Algorithms", "Machine Learning", "Distributed systems", "Cybersecurity"}));
+    }
+    
     @Autowired
     public void setFoundationModules() {
         courses.setFoundationCourses(FoundationCourses);
